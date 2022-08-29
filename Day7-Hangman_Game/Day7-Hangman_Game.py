96fc749757a44acd02d0f21f2d3bcff2416caf25
terminalJunki3 <webapptesterpa@gmail.com>
2022-08-29 09:44:50 -0400

    Hangman - add logic for win condition,

--- Day7-Hangman_Game/Day7-Hangman_Game.py
+++ Day7-Hangman_Game/Day7-Hangman_Game.py
@@ -1,9 +1,31 @@
+import random
 #Step 1
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# Randomly choose a word from the word_list.
+chosen_word = word_list[random.randint(0, len(word_list) - 1)]
+answer = list()
+for i in range(0, len(chosen_word)):
+    answer.append("_")
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+all_letters_guessed = False
+while not all_letters_guessed:
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    print(f"Chosen word is: {chosen_word}")
+
+    # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input("Take a guess: ").lower()
+    # Create an empty List called display.
+    # Loop through each position in the chosen_word;
+    for i, letter in enumerate(chosen_word):
+        if letter == guess:
+            answer[i] = guess
+
+    print(answer)
+    if "_" not in answer:
+        print("You Win! Game Over. ")
+        all_letters_guessed = True
+
+
+
